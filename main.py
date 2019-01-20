e92ed0db464623a1b2227272fcb258698905c042
ameya mellacheruvu <ameyacapps@gmail.com>
2019-01-19 19:08:27 -0800

    pages layed out

--- main.py
+++ main.py
@@ -7,24 +7,30 @@ jinja_env = jinja2.Environment(
    extensions=['jinja2.ext.autoescape'],
    autoescape=True)
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        main_template = jinja_env.get_template("templates/main.html")
-        self.response.write(main_template.render())
 
 class WelcomePage(webapp2.RequestHandler):
     def get(self):
-        welcome_template = jinja_env.get_template("welcome/.html")
+        welcome_template = jinja_env.get_template("templates/welcome.html")
         self.response.write(welcome_template.render())
 
+class DataEntryPage(webapp2.RequestHandler):
+    def get(self):
+        dataentry_template = jinja_env.get_template("templates/data.html")
+        self.response.write(dataentry_template.render())
+
+class PillPage(webapp2.RequestHandler):
+    def get(self):
+        pill_template = jinja_env.get_template("templates/pill.html")
+        self.response.write(pill_template.render())
 
 
 
 
 app = webapp2.WSGIApplication([
 
-   ('/', MainPage),
-   ('/welcome', WelcomePage),
+   ('/', WelcomePage),
+   ('/pillentry', PillPage),
+   ('/dataentry', DataEntryPage),
 
 
 ], debug=True)
