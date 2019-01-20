fc78ac5f5631ca85df8b5b1029c913ad7c8e5aaf
racheljn <rnguye12@lion.lmu.edu>
2019-01-20 11:29:04 -0800

    stuff

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,5 @@
 
 ///////////////Run at Program Start only
 const pillEntry = {
-  name:"birth controll",note:"take at night",expirydate:"1/20/2019", time:"8:30 PM"
+  name:"birth control",note:"take at night",expirydate:"1/20/2019", time:"8:30 PM"
 };
